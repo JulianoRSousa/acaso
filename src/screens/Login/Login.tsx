35e0763523f1ef45c9cc9403d1cbe66c05d77a0c
Juliano Sousa <julianorodsousa@gmail.com>
2023-09-26 11:08:47 -0400

    Raleway Font Setup

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,9 +1,10 @@
+import { colors } from "@/theme";
 import { View, Text } from "react-native";
 
 const Login: React.FC = () => {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Login</Text>
+            <Text style={{ fontSize: 32, fontFamily: 'Raleway-Bold', color: colors.Galaxy.White }}>Login</Text>
         </View>
     );
 }
